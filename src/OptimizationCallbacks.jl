ac4cfb4b90146ef03cea88233c3e940feb0d5840
Johanni Brea <jbrea@users.noreply.github.com>
2024-11-27 17:36:02 +0100

    add Evaluator

--- src/OptimizationCallbacks.jl
+++ src/OptimizationCallbacks.jl
@@ -7,7 +7,7 @@ using ConcreteStructs
 using Printf
 using JLD2
 
-export Callback, TimeTrigger, IterationTrigger, EventTrigger, LogProgress, CheckPointSaver
+export Callback, TimeTrigger, IterationTrigger, EventTrigger, LogProgress, CheckPointSaver, Evaluator
 
 """
     Callback(trigger, function; t = 0, extra = nothing, stop = (_, _, _, _) -> false)
@@ -347,5 +347,54 @@ function (cb::LogProgress)(state, value, t, _)
     end
     @printf "%7i | %11.6g | %11.6g | %11.6g\n" t value cb.lowest cb.highest
 end
+
+"""
+    Evaluator(f; T = Float64, label = :evaluation)
+
+Evaluate function `f` on `state` and store it in `evaluations`.
+
+### Example
+
+```jldoctest
+julia> using Optimization
+
+julia> using OptimizationCallbacks
+
+julia> import ForwardDiff
+
+julia> function rosenbrock(x, p)
+           (p[1] - x[1])^2 + p[2] * (x[2] - x[1]^2)^2
+       end
+rosenbrock (generic function with 1 method)
+
+julia> optf = OptimizationFunction(rosenbrock, AutoForwardDiff());
+
+julia> prob = OptimizationProblem(optf, [0., 0.], [1., 100.]);
+
+julia> callback = Callback(IterationTrigger(5), Evaluator(x -> rosenbrock(x.u, [1., 90.])));
+
+julia> sol = solve(prob, Optimization.LBFGS(); callback);
+
+julia> callback.func.evaluations
+5-element Vector{Float64}:
+ 0.45989873460740843
+ 0.16216539624081874
+ 0.024525435426304077
+ 0.0009100921964470193
+ 1.0256411872737028e-13
+```
+"""
+struct Evaluator{E,F}
+    label::Symbol
+    evaluations::E
+    f::F
+end
+function Evaluator(f; T = Float64, label = :evaluation)
+    Evaluator{Vector{T},typeof(f)}(label, T[], f)
+end
+function (ev::Evaluator)(state, _, _, _)
+    push!(ev.evaluations, ev.f(state))
+end
+
 
 end # module
